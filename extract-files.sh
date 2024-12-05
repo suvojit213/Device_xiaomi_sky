a87e4c78d023ad2c0a4b103e1e12e9df3526504b
Arian <arian.kulmer@web.de>
2024-12-05 20:33:36 +0000

    sky: Patch libcamximageformatutils to get rid of ndk_platform qti display config dependency
    Change-Id: I99bdcfe7ec1e0e4727c702f7ef8758c453f4424a

--- extract-files.sh
+++ extract-files.sh
@@ -87,6 +87,9 @@ function blob_fixup() {
     vendor/lib64/hw/fingerprint.fpc.default.so)
         "${PATCHELF}" --replace-needed "com.fingerprints.extension@1.0.so" "com.fingerprints.extension@1.0_vendor.so" "${2}"
         ;;
+    vendor/lib64/libcamximageformatutils.so)
+        "${PATCHELF}" --replace-needed "vendor.qti.hardware.display.config-V2-ndk_platform.so" "vendor.qti.hardware.display.config-V2-ndk.so" "${2}"
+        ;;
     vendor/lib64/libqshcamera.so)
         "${PATCHELF}" --replace-needed "libprotobuf-cpp-lite-3.9.1.so" "libprotobuf-cpp-full-3.9.1.so" "${2}"
         ;;
