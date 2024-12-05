ecfb220eacf0e2cc347b7ee553aae4c04bbd5f36
Lostark13 <tushar9915@gmail.com>
2024-12-05 20:33:36 +0000

    sky: Fix vendor.qti.hardware.fm ELF checks
    Change-Id: I3ade37b90b2fbb496fd1ff4fd00eb7c51ae4480e

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -44,6 +44,7 @@ function lib_to_package_fixup_vendor_variants() {
     case "$1" in
         vendor.qti.hardware.dpmservice@1.0 | \
         vendor.qti.hardware.dpmservice@1.1 | \
+        vendor.qti.hardware.fm@1.0 | \
         vendor.qti.hardware.qccsyshal@1.0 | \
         vendor.qti.hardware.qccsyshal@1.1 | \
         vendor.qti.hardware.qccvndhal@1.0 | \
